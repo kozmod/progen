43c85cc9cc4337a953920d764a56636d5c39ee8a
kozmod <gizmod@bk.ru>
2023-01-24 09:05:15 +0300

    change version generation for src builds (aka `Semantic Version`)

--- internal/version.go
+++ internal/version.go
@@ -1,20 +1,67 @@
 package internal
 
-import "runtime/debug"
+import (
+	"runtime/debug"
+	"strings"
+	"time"
 
-const (
-	_defaultVersion = "v0.0.0-local"
+	"github.com/kozmod/progen/internal/entity"
 )
 
-var Version string
+const (
+	_defaultVersion    = "v0.0.0"
+	_sourceBuildSuffix = ".src"
+
+	_debugRevisionKey = "vcs.revision"
+	_debugTimeKey     = "vcs.time"
+	_debugTimeLayout  = "20060102150405"
+
+	_versionSeparator = entity.Dash
+)
+
+var (
+	Version string
+)
 
 func GetVersion() string {
-	if Version != "" {
-		return Version
+	if v := strings.TrimSpace(Version); Version != entity.Empty {
+		return v
 	}
+
+	var sb strings.Builder
+	sb.WriteString(_defaultVersion)
+
 	version, ok := debug.ReadBuildInfo()
-	if ok && version.Main.Version != "(devel)" && version.Main.Version != "" {
-		return version.Main.Version
+	if ok {
+		values := make(map[int]string, 2)
+		for _, setting := range version.Settings {
+			key, value := setting.Key, strings.TrimSpace(setting.Value)
+			if value == entity.Empty {
+				continue
+			}
+
+			switch key {
+			case _debugTimeKey:
+				versionTime, err := time.Parse(time.RFC3339, value)
+				if err != nil {
+					continue
+				}
+				values[1] = versionTime.Format(_debugTimeLayout)
+			case _debugRevisionKey:
+				values[2] = value
+			}
+		}
+
+		for i := 1; i <= 2; i++ {
+			value, ok := values[i]
+			if !ok {
+				continue
+			}
+			sb.WriteString(_versionSeparator)
+			sb.WriteString(value)
+		}
 	}
-	return _defaultVersion
+
+	sb.WriteString(_sourceBuildSuffix)
+	return sb.String()
 }
